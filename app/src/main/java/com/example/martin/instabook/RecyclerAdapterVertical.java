9f96da2f5bb7da6b41e7500c87f8da43a6550ad1
tehmarkerer <mondrasik36@gmail.com>
2018-12-12 22:44:22 +0100

    pridali sa obrazky a trochu style a este email sending

--- app/src/main/java/com/example/martin/instabook/RecyclerAdapterVertical.java
+++ app/src/main/java/com/example/martin/instabook/RecyclerAdapterVertical.java
@@ -3,6 +3,7 @@ package com.example.martin.instabook;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Paint;
 import android.graphics.drawable.ColorDrawable;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
@@ -15,6 +16,8 @@ import android.widget.Toast;
 import com.example.martin.instabook.model.PostModel;
 import com.facebook.drawee.view.SimpleDraweeView;
 
+import org.w3c.dom.Text;
+
 import java.net.URL;
 import java.util.Random;
 
@@ -24,6 +27,7 @@ public class RecyclerAdapterVertical extends RecyclerView.Adapter<RecyclerViewHo
     private Context context;
 
     public static final String URL_KEY = "com.example.martin.instabook.URLKEY";
+    private final String userPlaceholderImageUrl = "http://oi63.tinypic.com/5w7k7.jpg";
 
 
     public RecyclerAdapterVertical(Context context, UserView dataset) {
@@ -65,33 +69,61 @@ public class RecyclerAdapterVertical extends RecyclerView.Adapter<RecyclerViewHo
 
         SimpleDraweeView imgView = recyclerViewHolder.view.findViewById(R.id.imgView);
 
-        imgView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                onImageClick(dataset.getMedia().get(index-1));
-            }
-        });
+
 
         TextView txtAuthor = recyclerViewHolder.view.findViewById(R.id.txtAuthor);
+        TextView postedOn = recyclerViewHolder.view.findViewById(R.id.postedOnText);
         TextView txtDate = recyclerViewHolder.view.findViewById(R.id.txtDate);
+        TextView txtNumPostsUser = recyclerViewHolder.view.findViewById(R.id.numberOfPostsUserPostedProfileId);
+        TextView txtEmailContactHint = recyclerViewHolder.view.findViewById(R.id.emailTextContactHintId);
+        TextView txtEmailContact = recyclerViewHolder.view.findViewById(R.id.emailTextContactId);
 
         if (i == 0) {
-            imgView.setVisibility(View.INVISIBLE);
+            // imgView.setVisibility(View.INVISIBLE);
+            imgView.setImageURI(userPlaceholderImageUrl);
+            txtEmailContactHint.setVisibility(View.VISIBLE);
+            txtEmailContact.setVisibility(View.VISIBLE);
             try {
                 txtAuthor.setText(this.dataset.getUserModel().getUsername());
-                txtDate.setText("profil");
+                postedOn.setVisibility(View.INVISIBLE);
+                txtDate.setText("This User registered on : " + this.dataset.getUserModel().dateToStr());
+                txtNumPostsUser.setText("Number of Posts posted : " + this.dataset.getUserModel().getNumberOfPosts());
+                txtEmailContactHint.setText("You can contact user directly via registed email");
+                txtEmailContact.setText(this.dataset.getUserModel().getEmail());
+                txtEmailContact.setPaintFlags(txtEmailContact.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+                txtEmailContact.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent emailIntent = new Intent(Intent.ACTION_SEND);
+                        emailIntent.setType("text/plain");
+                        emailIntent.putExtra(Intent.EXTRA_EMAIL, new String[] {dataset.getUserModel().getEmail()});
+                        emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Message from Instabook by User : " + FirebaseHelpers.getUser().getDisplayName());
+                        emailIntent.putExtra(Intent.EXTRA_TEXT, "Hello, try Instabook ;)");
+
+                        context.startActivity(Intent.createChooser(emailIntent,"Send email..."));
+                    }
+                });
             }catch (Exception e){
                 txtAuthor.setText("Loading...");
+                txtNumPostsUser.setText("Loading...");
                 txtDate.setText("profil");
             }
         } else {
-
+            imgView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    onImageClick(dataset.getMedia().get(index-1));
+                }
+            });
             imgView.setVisibility(View.VISIBLE);
+            postedOn.setVisibility(View.VISIBLE);
+            txtEmailContact.setVisibility(View.INVISIBLE);
+            txtEmailContactHint.setVisibility(View.INVISIBLE);
             try {
 
                 txtAuthor.setText(this.dataset.getUserModel().getUsername());
                 txtDate.setText(this.dataset.getMedia().get(i-1).dateToStr());
-
+                txtNumPostsUser.setText("");
                 if (this.dataset.getMedia().get(i-1).getType().equals(MainActivity.PHOTO_TYPE)) {
                     imgView.setImageURI(MainActivity.DOWNLOAD_URL + this.dataset.getMedia().get(i-1).getImageUrl());
                 }
